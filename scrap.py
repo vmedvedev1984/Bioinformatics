be6b5d1c14c746372e5f0e1171e70991e313420e
Vladimir S. Medvedev <stalker1984@mail.ru>
2025-07-07 13:29:28 +0300

    Update scrap.py
    Add aligment

--- scrap.py
+++ scrap.py
@@ -2,8 +2,8 @@ import wget
 #import ssl
 import requests
 from bs4 import BeautifulSoup
-
-for i in range(12000, 20000):
+'''
+for i in range(14559, 20000):
     print(i)
     url = f'https://www.addgene.org/{i}/sequences/'
     page = requests.get(url)
@@ -30,30 +30,52 @@ for i in range(12000, 20000):
         #links = soup.find_all('a', class_='genbank-file-download')[0].get('href')
         print(i)
         print(plasmid_name)
-        plasmid_name = plasmid_name.replace("/", '_').replace(":", '_').replace("'","_").replace('"',"")
+        plasmid_name = plasmid_name.replace("/", '_').replace(":", '_').replace("'","_").replace('"',"").replace('>',"")
         r = requests.get(links)
         with open(f'D:\\plasmid\\{plasmid_name}.gbk', "wb") as file:
             file.write(r.content)
-        
+
 '''
-url = 'https://www.addgene.org/54506/sequences/'
 
+from Bio import SeqIO
 
-print(soup)
-page = requests.get(url)
-print(page.status_code)
-plasmid_name = []
-seqences = []
-soup = BeautifulSoup(page.text, "html.parser")
-#print(soup)
-plasmid_name = repr(soup.find_all('span', class_='material-name')[0].string)
-links = soup.find_all('a', class_='genbank-file-download')[0].get('href')
-#current_link = soup.find_all().get('href')
-print(plasmid_name)
-print(links)
+Plasmid_file = "D:\plasmid\_223 pCS EGFP DEST_.gbk"
 
-r = requests.get(links)
-with open(f'D:\\plasmid\\{plasmid_name}.gbk', "wb") as file:
-    file.write(r.content)
-'''
-#wget.download('https://media.addgene.org/snapgene-media/v3.0.0/sequences/393242/e51691b8-d669-40be-9df7-60f59e5d84f5/addgene-plasmid-54506-sequence-393242.gbk', out = "D:\\plasmid\\")
+for seq_record in SeqIO.parse(Plasmid_file, "genbank"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
+
+identifiers = [seq_record.id for seq_record in SeqIO.parse(Plasmid_file, "genbank")]
+print(identifiers)
+
+records = list(SeqIO.parse(Plasmid_file, "genbank"))
+
+print("Found %i records" % len(records))
+
+print("The first record")
+first_record = records[0] 
+print(first_record.id)
+print(repr(first_record.seq))
+print(len(first_record))
+
+all_species = [seq_record.annotations["comment"] for seq_record in SeqIO.parse(Plasmid_file, "genbank")]
+print(all_species)
+
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from Bio import AlignIO
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("Pairwise Sequence Alignment using `pairwise2` with Custom Scoring:\n")
+
+seq1 = "AAAATG"
+seq2 = str(all_species)
+
+alignments = pairwise2.align.globalms(seq1, seq2, 2, -1, -0.5, -0.1)
+for alignment in alignments:
+    print(format_alignment(*alignment))
