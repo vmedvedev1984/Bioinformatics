6b7d823848d2998dd936eebb98ae54c1233825b2
Vladimir S. Medvedev <stalker1984@mail.ru>
2025-07-25 11:42:17 +0300

    Update BS_test.py
    Add BS

--- BS_test.py
+++ BS_test.py
@@ -65,3 +65,114 @@ tag['id']
 # KeyError: 'id'
 tag.get('id')
 # None
+
+#Multi-valued attributes
+css_soup = BeautifulSoup('<p class="body"></p>', 'html.parser')
+css_soup.p['class']
+# ['body']
+
+css_soup = BeautifulSoup('<p class="body strikeout"></p>', 'html.parser')
+css_soup.p['class']
+# ['body', 'strikeout']
+
+rel_soup = BeautifulSoup('<p>Back to the <a rel="index first">homepage</a></p>', 'html.parser')
+rel_soup.a['rel']
+# ['index', 'first']
+rel_soup.a['rel'] = ['index', 'contents']
+print(rel_soup.p)
+# <p>Back to the <a rel="index contents">homepage</a></p>
+
+id_soup = BeautifulSoup('<p id="my id"></p>', 'html.parser')
+id_soup.p['id']
+# 'my id'
+
+no_list_soup = BeautifulSoup('<p class="body strikeout"></p>', 'html.parser', multi_valued_attributes=None)
+no_list_soup.p['class']
+# 'body strikeout'
+
+id_soup.p['id']
+# 'my id'
+id_soup.p.get_attribute_list('id')
+# ["my id"]
+
+xml_soup = BeautifulSoup('<p class="body strikeout"></p>', 'xml')
+xml_soup.p['class']
+# 'body strikeout'
+
+class_is_multi= { '*' : 'class'}
+xml_soup = BeautifulSoup('<p class="body strikeout"></p>', 'xml', multi_valued_attributes=class_is_multi)
+xml_soup.p['class']
+# ['body', 'strikeout']
+
+from bs4.builder import builder_registry
+builder_registry.lookup('html').DEFAULT_CDATA_LIST_ATTRIBUTES
+
+soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', 'html.parser')
+tag = soup.b
+tag.string
+# 'Extremely bold'
+type(tag.string)
+# <class 'bs4.element.NavigableString'>
+
+unicode_string = str(tag.string)
+unicode_string
+# 'Extremely bold'
+type(unicode_string)
+# <type 'str'>
+
+tag.string.replace_with("No longer bold")
+tag
+# <b class="boldest">No longer bold</b>
+
+doc = BeautifulSoup("<document><content/>INSERT FOOTER HERE</document", "xml")
+footer = BeautifulSoup("<footer>Here's the footer</footer>", "xml")
+doc.find(text="INSERT FOOTER HERE").replace_with(footer)
+# 'INSERT FOOTER HERE'
+print(doc)
+# <?xml version="1.0" encoding="utf-8"?>
+# <document><content/><footer>Here's the footer</footer></document>
+
+soup.name
+# '[document]'
+
+######   Special strings    #####
+
+markup = "<b><!--Hey, buddy. Want to buy a used parser?--></b>"
+soup = BeautifulSoup(markup, 'html.parser')
+comment = soup.b.string
+type(comment)
+# <class 'bs4.element.Comment'>
+
+comment
+# 'Hey, buddy. Want to buy a used parser'
+
+print(soup.b.prettify())
+# <b>
+#  <!--Hey, buddy. Want to buy a used parser?-->
+# </b>
+
+#######     Navigating the tree     #######
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+soup.find("head")
+# <head><title>The Dormouse's story</title></head>
+
+soup.find("head")
+# <head><title>The Dormouse's story</title></head>
+
