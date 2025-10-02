904aa476504b2cf3b9173c6a63a7a5f6c1619662
Vladimir S. Medvedev <stalker1984@mail.ru>
2025-10-02 10:39:10 +0300

    Update scrap_file.py

--- scrap_file.py
+++ scrap_file.py
@@ -1,1 +1,13 @@
+import subprocess
+import wget
+import ssl
+import requests
+
+for i in range(10000, 40000):
+    print(i)
+    url = f'https://raw.githubusercontent.com/SciCrunch/RRID-Instruments/main/PDF/SCR_0{i}.pdf'
+    if requests.get(url).status_code == 200:
+        #url = f'https://raw.githubusercontent.com/SciCrunch/RRID-Instruments/main/PDF/SCR_0{i}.pdf'
+        filename = wget.download(url, out = "D:\\Manual\\")
+        print(f"Downloaded: {filename}")
 
